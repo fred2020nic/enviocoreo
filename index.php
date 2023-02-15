bd38e771418ae61ac66e165aa5c8586d5425416c
fred2020nic <60634761+fred2020nic@users.noreply.github.com>
2023-02-14 20:49:34 -0600

    Envio_correo_phpMailer

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require 'vendor/phpmailer/phpmailer/src/Exception.php';
+    require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+    require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+    // require 'PHPMailer/src/Exception.php';
+    // require 'PHPMailer/src/PHPMailer.php';
+    // require 'PHPMailer/src/SMTP.php';
+    // if($consulta=="1"){
+
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+       
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+                )
+            );
+            //Server settings
+            $mail->SMTPDebug = 0;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.office365.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username='soporte.fepsoft@outlook.com';//este debe ir en el address?
+            $mail->Password='#pipes2023#';                            // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('soporte.fepsoft@outlook.com', 'Freddy');
+            $mail->addAddress('fep.200978@gmail.com', 'Freddy');     // Add a recipient
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Prueba de Correo';
+            $mail->Body    = 'De HotMail a Gmail';
+
+            $mail->send();
+            // echo '1';
+        
+     {
+    //     echo '2';
+     }
+
+
+?>
